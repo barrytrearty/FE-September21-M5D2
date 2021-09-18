b252d6378be9570c106a6bc77e34f27eca0321fa
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-09-18 20:07:49 +0100

    upload photo connected to front end

--- src/authors/validation.js
+++ src/authors/validation.js
@@ -4,6 +4,6 @@ export const authorValidation = [
   body("name").exists().withMessage("name required"),
   body("surname").exists().withMessage("surname required"),
   body("email").exists().withMessage("email required"),
-  body("dateOfBirth").exists().withMessage("dateOfBirth required"),
+  // body("dateOfBirth").exists().withMessage("dateOfBirth required"),
   //   body("avatar").exists().withMessage("avatar"),
 ];
